2e71c643f47cc8fde071e94274996f22f68a601d
kpandita2406 <pandita.kanishka0@gmail.com>
2023-12-12 01:23:46 +0530

    testing PR review changes

--- gen-ai-pr-review/src/pages/Contact.jsx
+++ gen-ai-pr-review/src/pages/Contact.jsx
@@ -1,5 +1,10 @@
 const Contact = () => {
-    return <h1>Contact Me</h1>;
-  };
-  
-  export default Contact;
+    return (
+        <>
+            <h1>Contact Me</h1>
+            <div>New contact details</div>
+        </>
+    );
+};
+
+export default Contact;
